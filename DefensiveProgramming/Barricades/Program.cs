beba71196039682bff52f903bffc117e9fca664d
Boris Milašinović <boris.milasinovic@fer.hr>
2022-04-07 21:13:27 +0200

    PI05 i pripadajući primjeri (defenzivno programiranje, konfiguracijske datoteke, praćenje traga, kovarijantnost i kontravarijantnosti, delegati)

---
+++ DefensiveProgramming/Barricades/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Barricades
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.OutputEncoding = Encoding.UTF8;
+
+      Course c = new Course("PI");      
+      c["Pero"] = 2;
+      c["Ana"] = 5;
+      c["Ivan"] = 5;
+      c["Marko"] = 1;
+      c["Luka"] = 3;
+      //p["Marija"] = -7; //should throw an exception
+      Console.WriteLine("Average grade: " + c.AverageGrade());
+      Console.WriteLine("Petar's grade: " + c["Petar"]); //prints -1
+    }
+  }
+}
