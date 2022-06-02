d4f34ba180e23e5c671191edd19f8598bcb8c3f5
Boris Milašinović <boris.milasinovic@fer.hr>
2022-06-02 21:55:11 +0200

    Testiranje

---
+++ Testing/MVC.IntegrationTests/HomeControllerShould.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MVC.IntegrationTests
+{
+  public class HomeControllerShould : IClassFixture<WebApplicationFactory<Program>>
+  {
+    private readonly WebApplicationFactory<Program> factory;
+
+    public HomeControllerShould(WebApplicationFactory<Program> factory) 
+    {
+      this.factory = factory;
+    }
+    
+    [Theory]
+    [Trait("Category", "Integration Tests")]
+    [InlineData("Home/Index")]
+    [InlineData("")]
+    public async Task ServeHomePage(string url)
+    {      
+      var client = factory.CreateClient();
+      var response = await client.GetAsync(url);
+      response.StatusCode.Should().Be(HttpStatusCode.OK);
+      string content = await response.Content.ReadAsStringAsync();
+      content.Should().Contain("FER");
+    }
+  }
+}
